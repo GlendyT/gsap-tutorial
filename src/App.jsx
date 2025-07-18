0609ab41f4de94dc34c191260d096bedc46847cf
GlendyT <glendytuyuc@gmail.com>
2025-07-17 19:17:05 -0600

    adding more animations for a layout of the component art

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,8 +12,10 @@ const App = () => {
   return (
     <main>
       <Navbar />
-      <Hero/>
-      <div className="h-dvh bg-black"></div>
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art/>
     </main>
   );
 };
